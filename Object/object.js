9f3c37a5d5dc9dec35a625510e68d6003551d785
Ananya Shukla <ananyashukla.ss290@gmail.com>
2026-03-05 18:52:26 +0530

    object

--- Object/object.js
+++ Object/object.js
@@ -48,3 +48,164 @@ Date Of Journey:${trainTicketInformation.DateOfJourney}<br>
 
 
 
+
+// Task 3
+// Create one Object to Hold Information of School.
+// Fields will be SchoolName, PrincipleName, ContactNo, and Address.
+// Print whole Object in console
+// after that add one more Field SchoolEmail with some email.
+// Again print updated object.
+ 
+school={
+    SchoolName:"kv", PrincipleName:'vaishnavi', ContactNo:73952076527,  Address:"balrampur"
+}
+ 
+console.log(school)
+ 
+ 
+// Task 4
+// Create one Object to Hold SupportTicket data
+// Properties will be TicketNo, CreatedBy, CreatedDate, AssigntoEmployeeName,
+// TicketDescription.
+// Keep AssigntoEmployeeName filed with empty value.
+// Print a dropdown with Employee Name.
+// ON Assign Button add selected employee name to AssigntoEmployeeName.
+// Print Whole Object after assigning employee in Console.
+ 
+SupportTicket={
+    TicketNo:1234, CreatedBy:'12-04-2025', CreatedDate:'13-04-2026', AssigntoEmployeeName:"" ,TicketDescription:"Software negineer"
+}
+ 
+var t =''
+function OnCHANGE(event){
+    t = event.target.value
+    console.log(t)
+   
+}
+ 
+function clickkaro(){
+ SupportTicket.AssigntoEmployeeName = t;
+ console.log(SupportTicket)
+}
+ 
+ 
+// Create employee Object with fields name, contactNo, deptName, teamLeadName,technology
+// Create a form for above fileds like below
+// on Save Button - add values to perticular field from textbox
+// on Print button - print whole object into p tag
+// on clear button - empty value of all fields from object and print updated Object again
+ 
+ 
+employee ={
+    fieldsname:"a", contactNo:1, deptName:"ananya", teamLeadName:"aanan",technology:"asdsfd"
+}
+ 
+function resetEmployee(){
+    employee.fieldsname ="",
+    employee.contactNo ="",
+    employee.deptName ="",
+    employee.teamLeadName ="",
+    employee.technology =""
+}
+ 
+function savekaro(){
+    employee.fieldsname =document.getElementById("name").value,
+    employee.contactNo =document.getElementById("conatct").value,
+    employee.deptName =document.getElementById("dept").value,
+    employee.teamLeadName =document.getElementById("teamlead").value,
+    employee.technology =document.getElementById("tech").value
+    console.log(employee)
+}
+ 
+function printEmp(){
+    document.getElementById("display").innerText =
+    `
+    ${employee.fieldsname}
+    ${employee.contactNo},
+    ${employee.deptName},
+    ${employee.teamLeadName},
+    ${employee.teamLeadName}
+ 
+ 
+    `
+}
+ 
+ 
+// Task - User Profile Manager
+// Create object to store user profile:
+// Basic info: firstName, lastName, email, phone
+// Address: street, city, state, pincode
+// Preferences: theme, language, notifications
+// Create form with fields for each property
+// Buttons: "Save Profile", "View Profile", "Update Field", "Clear All"
+// Display profile in JSON format in console and formatted HTML
+ 
+UserProfile ={
+    firstName:"", lastName:"", email:"", phone:"", address:{
+        street:"", city:"", state:"", pincode:""
+    },Preferences:{
+        theme:"", language:"", notifications:""
+    }
+}
+ 
+ 
+function saveProfile(){
+UserProfile.firstName = document.getElementById("firstName").value
+UserProfile.lastName = document.getElementById("lastName").value
+UserProfile.email = document.getElementById("email").value
+UserProfile.phone = document.getElementById("phone").value
+ 
+UserProfile.address.street = document.getElementById("street").value
+UserProfile.address.city = document.getElementById("city").value
+UserProfile.address.state = document.getElementById("state").value
+UserProfile.address.pincode = document.getElementById("pincode").value
+ 
+UserProfile.Preferences.theme = document.getElementById("theme").value
+UserProfile.Preferences.language = document.getElementById("language").value
+UserProfile.Preferences.notifications = document.getElementById("notifications").value
+ 
+console.log(UserProfile)
+}
+ 
+function viewProfile(){
+ 
+    document.getElementById("output").innerText = JSON.stringify(UserProfile)
+ 
+}
+ 
+function resetProfile(){
+ 
+    UserProfile.firstName=""
+    UserProfile.lastName=""
+    UserProfile.email=""
+    UserProfile.phone=""
+   
+    UserProfile.address.street=""
+    UserProfile.address.city=""
+    UserProfile.address.state=""
+    UserProfile.address.pincode=""
+   
+    UserProfile.Preferences.theme=""
+    UserProfile.Preferences.language=""
+    UserProfile.Preferences.notifications=""
+   
+    }
+function updateProfile(){
+ 
+        UserProfile.firstName = document.getElementById("firstName").value
+        UserProfile.lastName = document.getElementById("lastName").value
+        UserProfile.email = document.getElementById("email").value
+        UserProfile.phone = document.getElementById("phone").value
+       
+        UserProfile.address.street = document.getElementById("street").value
+        UserProfile.address.city = document.getElementById("city").value
+        UserProfile.address.state = document.getElementById("state").value
+        UserProfile.address.pincode = document.getElementById("pincode").value
+       
+        UserProfile.Preferences.theme = document.getElementById("theme").value
+        UserProfile.Preferences.language = document.getElementById("language").value
+        UserProfile.Preferences.notifications = document.getElementById("notifications").value
+       
+        viewProfile()
+       
+        }
